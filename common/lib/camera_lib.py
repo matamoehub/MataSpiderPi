621cffca679ba1782ff529ce9a80371c14fb348d
John Clegg <john@MacBook-Pro-2.local>
2026-04-04 10:51:56 +1300

    Further speed up SpiderPi arm motions

--- common/lib/camera_lib.py
+++ common/lib/camera_lib.py
@@ -7,8 +7,8 @@ from typing import Optional
 from arm_lib import LOOK, get_arm
 
 HEAD_PITCH = 0
-HEAD_DEFAULT_SECONDS = 0.06
-HEAD_MOTION_SECONDS = 0.07
+HEAD_DEFAULT_SECONDS = 0.03
+HEAD_MOTION_SECONDS = 0.035
 
 
 class Camera:
